4d15c3395031f20288c864275b25dcab161953f4
sandond77 <sandond77@gmail.com>
2025-05-28 16:19:08 -0700

    reworked menu json to fix prices and added missing prices

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const menu = require('./data/menu.json');
 
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 	res.send('Welcome to Not In-n-Out Burger Menu');
+});
+
+app.get('/menu', (req, res) => {
+	res.json(menu);
 });
 
 app.listen(port, () => {
