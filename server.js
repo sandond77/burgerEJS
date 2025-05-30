ea4b58de27449fcfedf73b0ac1f80a0fc7bab18c
sandond77 <sandond77@gmail.com>
2025-05-30 11:19:45 -0700

    removed unused variables from server.js and added redirection for random urls

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ const app = express();
 const port = 3000;
 const menu = require('./data/menu.json');
 
-const burgers = menu['burgers'];
-const fries = menu['fries'];
-const drinks = menu['drinks'];
-const secret = menu['secretMenu'];
-
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -17,8 +12,12 @@ app.get('/', (req, res) => {
 	res.render('./menu.ejs', { ...menu });
 });
 
-app.get('/menu', (req, res) => {
-	res.json(menu);
+app.get('/secret', (req, res) => {
+	res.render('./secret.ejs', { ...menu });
+});
+
+app.get(/^\/(.*)/, (req, res) => {
+	res.redirect('/');
 });
 
 app.listen(port, () => {
